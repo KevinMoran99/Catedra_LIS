9daa301669613e88286dd73a0efc094ef8ad30d6
Oscar Argueta <ooskarargueta08@gmail.com>
2018-02-19 16:06:48 -0600

    Actualizado header de detalles de juegos

--- public/views/gameDetail.php
+++ public/views/gameDetail.php
@@ -1,45 +1,58 @@
-
-<div class="row">
-    <div class="col m10 offset-m1">
-        <div id="main" class="row">
-            <div class="col s12 l7">
-                <a href="#"><img class="responsive-img" src="https://picsum.photos/600/400"></a>
-            </div>
-            <div class="col s12 l3">
-                Info del juego o no se
-            </div>
-        </div>
-        
-        
-        
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="comment" type="text">
-                <label for="comment">Hacer comentario</label>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col s12">
-                <div class="card horizontal">
-                    Comentarios
-                </div>
+<link rel="stylesheet" href="css/gameDetail.css">
+<div class="navbar-fixed">
+    <nav class="left-align">
+        <h5>Kevin no lo ha jugado</h5>
+    </nav>
+</div>
+<div class="row" id="gameDetailHeader">
+    <div class="gameBackground"></div>
+    <div class="col s12 m6 l4">
+        <div class="card">
+            <div class="row">
+                <img class="center" class="col push-s2" src="../web/img/example2.png" id="gameCover" />
             </div>
         </div>
         <div class="row">
-            <div class="col s12">
-                <div class="card horizontal">
-                    Comentarios
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col s12">
-                <div class="card horizontal">
-                    Comentarios
+            <div class="center col s12">
+                <div class="row">
+                    <strong class="col s6">
+                        <p class="right">Clasificacion</p>
+                    </strong>
+                    <img class="col s6 left" id="esrb" src="../web/img/esrb/esrb_rp.png" />
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="col s12 m6 l4" id="game-description">
+
+        <div id="info">
+            <h5>Descripcion</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor gravida tristique. Vestibulum est dolor,
+                vestibulum eget vulputate consectetur, vehicula sed est. Fusce efficitur, nunc in pharetra faucibus, leo
+                diam venenatis nunc, a </p>
+            <h5>Disponible para: </h5>
+            <ul>
+                <li>*Cualquier cosa que no tenga kevin</li>
+                <li>*Cualquier cosa que no tenga kevin x2</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col s12 m6 l4">
+
+        <h5 class="col s12 center">Rating promedio</h5>
+        <div class="col s12 center">
+            <i class="medium material-icons rate-star tooltipped" id="malo" data-tooltip="Malo" data-position="bottom" data-delay="50">star</i>
+            <i class="medium material-icons rate-star tooltipped" id="bueno" data-tooltip="Bueno" data-position="bottom" data-delay="50">star</i>
+            <i class="medium material-icons rate-star tooltipped" id="muy-bueno" data-tooltip="Muy bueno" data-position="bottom" data-delay="50">star</i>
+            <i class="medium material-icons rate-star tooltipped" id="excelente" data-tooltip="Excelente" data-position="bottom" data-delay="50">star</i>
+        </div>
+        <a class="col s12 center">Ver todos los rating</a>
+        <div class="row">
+            <button class="btn col s12 white black-text">$0.00</button>
+            <button class="btn col s12 blue">Al carrito</button>
+        </div>
+    </div>
 </div>
-        
+<hr>
--- public/views/games.php
+++ public/views/games.php
@@ -2,11 +2,12 @@
 
  <div class="row">
  <?php
+ $url = "attach('gameDetail');";
  for($i = 0; $i<=10; $i++){
      echo '<div class="col s6 m3 l3">
              <div class="card">
                  <div class="card-image">
-                     <img src="../web/img/example.png">
+                     <img src="../web/img/example2.png" onclick="'.$url.'">
                      <span class="card-title">Nombre Juego</span>
                  </div>
              </div>
