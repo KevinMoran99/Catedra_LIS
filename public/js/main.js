3a07ba6b3822bd0c890e366431799eca03447e37
Ara Mendez <ooskarargueta08@gmail.com>
2018-02-23 04:23:56 -0600

    Fix final

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-    /*inicializa el carousel de la pagina principal*/
-    function startCarousel() {
-        $('.carousel.carousel-slider').carousel({ fullWidth: true });
-        $('#game1-banner').css("background-image", "url('../web/img/nierBanner.jpg')");
-        $('#game2-banner').css("background-image", "url('../web/img/bioshockBanner.jpg')");
-        $('#game3-banner').css("background-image", "url('../web/img/childoflightBanner.jpg')");
-        $('#game4-banner').css("background-image", "url('../web/img/darksidersBanner.jpg')");
-        setInterval(function() {
-            $('.carousel').carousel('next');
-        }, 5000);
-    }
-    $('#nextButton').find('i').click(function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $('.carousel').carousel('next');
-    });
+/*inicializa el carousel de la pagina principal*/
+startCarousel();
 
-    $('#prevButton').find('i').click(function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $('.carousel').carousel('prev');
-    });
+function startCarousel() {
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    $('#game1-banner').css("background-image", "url('../web/img/nierBanner.jpg')");
+    $('#game2-banner').css("background-image", "url('../web/img/bioshockBanner.jpg')");
+    $('#game3-banner').css("background-image", "url('../web/img/childoflightBanner.jpg')");
+    $('#game4-banner').css("background-image", "url('../web/img/darksidersBanner.jpg')");
+    setInterval(function() {
+        $('.carousel').carousel('next');
+    }, 5000);
+}
+$('#nextButton').find('i').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.carousel').carousel('next');
+});
+
+$('#prevButton').find('i').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.carousel').carousel('prev');
 });
