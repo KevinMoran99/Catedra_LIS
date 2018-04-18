08bea7789795510180b3deee775bb0d508a48b2a
krmorang@gmail.com <krmorang@gmail.com>
2018-04-18 00:02:36 -0600

    Gráfico de las clasificaciones hecho a lo vale riata pero me convence hasta ahora.

--- app/dashboard/views/stadistics.php
+++ app/dashboard/views/stadistics.php
@@ -2,6 +2,7 @@
 <div class="wrapper">
     <h4>Estadísticas</h4>
     <!--Gráficas de demostración-->
+
     <div class="row">
         <div class="col s12 m6">
             <canvas id="myChart1" width="400" height="400"></canvas>
@@ -16,6 +17,11 @@
         </div>
         <div class="col s12 m6">
             <canvas id="myChart4" width="400" height="400"></canvas>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 m6">
+            <canvas id="myChart5" width="400" height="400"></canvas>
         </div>
     </div>
 </div>
